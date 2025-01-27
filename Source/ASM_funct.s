71ef199d5b24caa8fe79fa6fa83c44511af5fbed
Cosimo <Sergi.Cosimo23@gmail.com>
2025-01-27 14:22:31 +0100

    SVC

--- Source/ASM_funct.s
+++ Source/ASM_funct.s
@@ -1,6 +1,7 @@
 			AREA asmfunct,CODE,READONLY
 			EXPORT prova
 			EXPORT ciao
+			EXPORT call_svc
 prova FUNCTION
 			PUSH {R4-R8,LR}
 			MOV R0,#5
@@ -13,5 +14,12 @@ ciao FUNCTION
 			MOV R0,#1
 			POP {R4-R8,PC}
 			ENDFUNC
-			
+
+call_svc  
+		    MOV r0,r13
+		    svc #0x000000FF
+		    NOP
+		    B .
+		    ENDP
+				
 			END
